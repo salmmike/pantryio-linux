343cf84bedf121ee9e6e79bf288383f437efcdba
Mike Salmela <salmela.mike@gmail.com>
2023-04-10 00:02:39 +0300

    Add helper scripts

---
+++ build/create_image.sh
@@ -0,0 +1,1 @@
+tmp-glibc/deploy/images/stm32mp15-disco/scripts/create_sdcard_from_flashlayout.sh tmp-glibc/deploy/images/stm32mp15-disco/flashlayout_pantry-io-image/optee/FlashLayout_sdcard_stm32mp157f-dk2-optee.tsv
---
+++ build/flash.sh
@@ -0,0 +1,1 @@
+sudo dd if=tmp-glibc/deploy/images/stm32mp15-disco/FlashLayout_sdcard_stm32mp157f-dk2-optee.raw of=/dev/sda bs=4M status=progress
